a765505fb11c5c80c3e30ff89ed97d9098ac5bc2
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-11-16 00:24:19 +0900

    🚀 First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# microcms-ts-client
---
+++ build.ts
@@ -0,0 +1,26 @@
+const { build } = require('esbuild')
+const { dependencies } = require('./package.json')
+
+const entryFile = 'src/index.ts'
+const shared = {
+  bundle: true,
+  entryPoints: [entryFile],
+  external: Object.keys(dependencies),
+  logLevel: 'info',
+  minify: true,
+  sourcemap: false,
+}
+
+build({
+  ...shared,
+  format: 'esm',
+  outfile: './dist/index.esm.js',
+  target: ['ES6'],
+})
+
+build({
+  ...shared,
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
+  target: ['ES6'],
+})
